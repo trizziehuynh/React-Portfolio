5e1f30413f80b6b1b656a8eab78fdaaa0f84e92b
Trizzie Huynh <putmind2822@gmail.com>
2023-04-26 16:59:48 -0700

    added the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,26 @@
-import Reat from "react";
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-const Footer=()=>{
-
-}
+const Footer = () => {
+  return (
+    <>
+      <Grid container justifyContent={"center"} spacing={2} padding={2}>
+        <Grid item>
+          <a href="https://github.com/trizziehuynh" target="_blank">
+            <GitHubIcon />
+          </a>
+        </Grid>
+        <Grid item>
+          <a href="https://www.linkedin.com/" target="_blank">
+            <LinkedInIcon />
+          </a>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default Footer;
